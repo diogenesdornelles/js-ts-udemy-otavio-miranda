e4c89bb8560bad4725bca823328d9875ef76cb25
@diogenesdornelles <diogenes.dornelles@gmail.com>
2022-10-02 00:41:55 -0300

    new lessons

---
+++ Aula_19_dados_primitivos/index.js
@@ -0,0 +1,13 @@
+// dados primitivos
+
+const nome = 'Jojo'; //str
+const num_1 = 1; //number
+const num_2 = 1.1; // number
+let amount; // undefined
+let lastName = null; //nulo
+const bool = true; // true or false
+
+const a = [1, 2] // array of numbers
+const b = [a] // array
+
+console.log(a, b) //
---
+++ Aula_20_operadores_aritmeticos_atribuicao_incremento/index.js
@@ -0,0 +1,32 @@
+/**
+ * Aritméticos 
+ * + - Adição e concatenação
+ * - - Subtração
+ * * - Multiplicação
+ * / - Divisão
+ * ** - Potenciação
+ * % - mod ou resto da divisão
+ *  */ 
+
+let contador = 1;
+contador++; // Operador de incremento. Soma ele mesmo duas vezes. Se posterior, chama a própria constante primeiro.
+++contador; // Se anterior, executa primeiro a operação.
+console.log(contador);
+
+let contador2 = 10;
+--contador2; // Operador de decremento.
+console.log(contador2);
+
+let contador3 = 0;
+contador3 += 5; // Como python. Serve para qualquer operador.
+contador3 += 5;
+contador3 += 5;
+console.log(contador3);
+
+const num1 = 1;
+const num2 = parseInt('2'); // Converte tipos. parseFloat etc
+const num3 = parseFloat('3.2'); //
+const num4 = Number('4.5'); //
+console.log(num1 + num2);
+
+
